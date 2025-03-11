660be44cd8e2b393b217a2df0bfb29fe872e4636
laurenlastoria <laurenlastoria2001@icloud.com>
2025-03-11 19:49:51 -0400

    Updated AppBase with connect button

--- FilterAlgorithmApp.m
+++ FilterAlgorithmApp.m
@@ -1,10 +1,13 @@
 classdef FilterAlgorithmApp < matlab.apps.AppBase
+    %% App Properties
     properties (Access = public)
         UIFigure            matlab.ui.Figure
         GridLayout          matlab.ui.container.GridLayout
         LoadButton          matlab.ui.control.Button
         ProcessButton       matlab.ui.control.Button
         ConnectButton       matlab.ui.control.Button
+        DisconnectButton    matlab.ui.control.Button
+        SearchButton        matlab.ui.control.Button
         StartButton         matlab.ui.control.Button
         StopButton          matlab.ui.control.Button
         SaveButton          matlab.ui.control.Button
@@ -16,6 +19,9 @@ classdef FilterAlgorithmApp < matlab.apps.AppBase
     properties (Access = private)
         Counter;
         Data;
+        DeviceName;
+        BLE;
+        BLEChar;
         AccelerometerValues;
         GyroscopeValues;
         MagnetometerValues;
@@ -27,6 +33,7 @@ classdef FilterAlgorithmApp < matlab.apps.AppBase
         FinalMag;
     end
     
+    %% Main Algorithm Functions
     methods (Access = private)
         function loadData(app)
             [file, path] = uigetfile('*.mat', 'Select Data File');
@@ -37,30 +44,33 @@ classdef FilterAlgorithmApp < matlab.apps.AppBase
             app.Data = load(fullfile(path, file));
         end
         
-        function a = processAccel(app, data)
+        function processAccel(app, data)
             a = swapbytes(typecast(reshape(uint8(data(:, 1:6)'), 1, []), "uint16"'));
             a = reshape(a, [3, 10])'; 
             a = double(a) * 4096 / 65535;
             a = a ./ [12.279 12.037 13.999];
             a = a * 9.81;
+            app.AccelerometerValues = [app.AccelerometerValues; a];
         end
                 
-        function g = processGyro(app, data)
+        function processGyro(app, data)
             g = swapbytes(typecast(reshape(uint8(data(:, 7:12)'), 1, []), "uint16"));
             g = reshape(g, [3, 10])';
             g = double(g) * 4000 / 32768;
             g = deg2rad(g);
+            app.GyroscopeValues = [app.GyroscopeValues; g];
         end
         
-        function hg = processHighGyro(app, data)
+        function processHighGyro(app, data)
             hg = swapbytes(typecast(reshape(uint8(data(:, 13:14)'), 1, []), "uint16"));
             hg = reshape(hg, [1, 10])';
             hg = double(hg) * 4096 / 65535;
             hg = hg / 0.0955;
             hg = deg2rad(hg);
+            app.HighGyroscopeValues = [app.HighGyroscopeValues; hg];
         end
         
-        function m = processMag(app, data) 
+        function processMag(app, data) 
             m = swapbytes(typecast(reshape(uint8(data(:, 15:20)'), 1, []), "uint16"));
             m = reshape(m, [3, 10])';
             m = uint32(m);
@@ -70,6 +80,7 @@ classdef FilterAlgorithmApp < matlab.apps.AppBase
             m = double(m) - 131072;
             m = m / 16384;
             m = m * 100;
+            app.MagnetometerValues = [app.MagnetometerValues; m];
         end
 
         function velocityValue = estimateVelocity(app, madgwick_quat, accel, gyro, mag)
@@ -100,31 +111,22 @@ classdef FilterAlgorithmApp < matlab.apps.AppBase
             fprintf('Velocity: %.2f m/s\n', velocityValue);
         end
 
-        function processData(app)
-            if isempty(app.Data)
-                uialert(app.UIFigure, 'No data loaded!', 'Error');
-                return;
-            end
+        function processData(app, src, ~)
+            % Reshape new data from the sensors into 10x21 matrix
+            % Preprocess the data for each sensor
+            sensorData = read(src, "oldest");
+            app.Data = [app.Data; sensorData];
+            processCounter(app, sensorData);
+            data = reshape(sensorData(1:210), [21, 10])';
+            processAccel(app, data);
+            processGyro(app, data);
+            processHighGyro(app, data);
+            processMag(app, data);
+            app.Time = [app.Time; datetime("now", "Format", "dd-MMM-uuuu HH:mm:ss.SSS")];
+        end
             
-            data = app.Data;
-            rawData = data.RawData;
-            
-            % Process data (same as original script, but optimized)
-            numRows = size(rawData, 1);
-            app.AccelerometerValues = [];
-            app.GyroscopeValues = [];
-            app.MagnetometerValues = [];
-            app.HighGyroscopeValues = [];
-            
-            for i = 1:numRows
-                fullData = reshape(rawData(i, 1:210), [21, 10])';
-                % Append processed data
-                app.AccelerometerValues = [app.AccelerometerValues; processAccel(app, fullData)];
-                app.GyroscopeValues = [app.GyroscopeValues; processGyro(app, fullData)];
-                app.MagnetometerValues = [app.MagnetometerValues; processMag(app, fullData)];
-                app.HighGyroscopeValues = [app.HighGyroscopeValues; processHighGyro(app, fullData)];
-            end
-            
+        
+        function getResults(app)   
             % Data corrections
             app.FinalAccel = app.AccelerometerValues - mean(app.AccelerometerValues(1:100, :), "omitnan") + [0 0 9.81];
             app.FinalGyro = (app.GyroscopeValues - mean(app.GyroscopeValues(1:100, :), "omitnan")) .* [1 -1 -1];
@@ -160,6 +162,10 @@ classdef FilterAlgorithmApp < matlab.apps.AppBase
             fprintf('Peak Velocity: %.2f m/s\n', peakVelocity);
             time = 0:2.5e-3:(size(app.VelocityArray, 1)-1)/400;
             app.plotResults(time, app.VelocityArray);
+
+            app.StartButton.Enable = "on";
+            app.StopButton.Enable = "off";
+            app.SaveButton.Enable = "on";
         end
         
         function plotResults(app, time, velocityArray)
@@ -169,45 +175,159 @@ classdef FilterAlgorithmApp < matlab.apps.AppBase
             title(app.Axes, 'Velocity over Time');
             grid(app.Axes, 'on');
         end
-    end
     
-    methods (Access = public)
-        function app = FilterAlgorithmApp()
-            app.UIFigure = uifigure('Name', 'Performance Tracking Hockey Puck App Results');
-            app.LoadButton = uibutton(app.UIFigure, 'Text', 'Load Data', 'Position', [20, 350, 100, 30], 'ButtonPushedFcn', @(~,~) app.loadData());
-            %app.ProcessButton = uibutton(app.UIFigure, 'Text', 'Process Data', 'Position', [140, 350, 100, 30], 'ButtonPushedFcn', @(~,~) app.processData());
-            app.StartButton = uibutton(app.UIFigure, 'Text', 'Start', 'Position', [140, 350, 100, 30], 'ButtonPushedFcn', @(~,~) app.processData());
-            app.Label = uilabel(app.UIFigure, 'Text', 'No file loaded', 'Position', [20, 320, 200, 30]);
-            app.Axes = uiaxes(app.UIFigure, 'Position', [20, 20, 400, 280]);
+        %% Button Functions
+        % Button pushed function: SearchButton
+        function SearchButtonPushed(app, event)
+            device = [];
+            app.Label.Text = "Searching for" + newline + "the Puck Sensor..."; 
+            while (isempty(device))
+                device = blelist("Name", "Puck Sensor");
+            end
+            app.DeviceName = device.Name;
+            app.Label.Text = app.DeviceName + newline + "detected";
+            app.ConnectButton.Enable = "on";
+        end
+        
+        % Button pushed function: ConnectButton
+        function ConnectButtonPushed(app, event)
+            app.BLE = ble(app.DeviceName);  
+            app.BLEChar = characteristic(app.BLE, ...
+                "0A361000-115B-4E3F-80E6-05D7F5F2730E", ...
+                "0A361001-115B-4E3F-80E6-05D7F5F2730E");
+            app.BLEChar.DataAvailableFcn = @(src, event)app.processData(src, event);           
+            app.Label.Text = "Connected to" + newline + app.DeviceName;
+            app.ConnectButton.Enable = "off";
+            app.DisconnectButton.Enable = "on";
+            app.StartButton.Enable = "on";
         end
 
-        function createComponents(app)
-            % Create ConnectButton
-            app.ConnectButton = uibutton(app.GridLayout, 'push');
-            app.ConnectButton.ButtonPushedFcn = createCallbackFcn(app, @ConnectButtonPushed, true);
-            app.ConnectButton.FontSize = 14;
-            app.ConnectButton.Enable = 'off';
-            app.ConnectButton.Layout.Row = 4;
-            app.ConnectButton.Layout.Column = 2;
-            app.ConnectButton.Text = 'Connect';
-        
-            % Create StartButton
-            app.StartButton = uibutton(app.GridLayout, 'push');
-            app.StartButton.ButtonPushedFcn = createCallbackFcn(app, @StartButtonPushed, true);
-            app.StartButton.FontSize = 14;
-            app.StartButton.Enable = 'off';
-            app.StartButton.Layout.Row = 11;
-            app.StartButton.Layout.Column = 2;
-            app.StartButton.Text = 'Start';
-        
-            % Create FinishButton
-            app.FinishButton = uibutton(app.GridLayout, 'push');
-            app.FinishButton.ButtonPushedFcn = createCallbackFcn(app, @FinishButtonPushed, true);
-            app.FinishButton.FontSize = 14;
-            app.FinishButton.Enable = 'off';
-            app.FinishButton.Layout.Row = 12;
-            app.FinishButton.Layout.Column = 2;
-            app.FinishButton.Text = 'Finish';
+        % Button pushed function: DisconnectButton
+        function DisconnectButtonPushed(app, event)
+            unsubscribe(app.BLEChar);
+            disconnect(app.BLE);
+            app.BLEChar = [];
+            app.BLE = [];
+            app.Time = [];
+            app.Label.Text = "Disconnected from" + newline + app.DeviceName;
+            app.ConnectButton.Enable = "on";
+            app.DisconnectButton.Enable = "off";
+            app.StartButton.Enable = "off";
         end
+
+        % Button pushed function: StartButton
+        function StartButtonPushed(app, event)
+            app.Time = datetime("now", "Format", "dd-MMM-uuuu HH:mm:ss.SSS");
+            app.Data = [];
+            app.Counter = app.Counter(end);
+            app.AccelerometerValues = [];
+            app.GyroscopeValues = [];
+            app.HighGyroscopeValues = [];
+            app.MagnetometerValues = [];
+            app.Label.Text = "Getting data from" + newline + app.DeviceName;
+            app.StartButton.Enable = "off";
+            app.StopButton.Enable = "on";
+        end
+
+        % Button pushed function: SaveButton
+        function SaveButtonPushed(app, event)
+            TimeVar = duration(app.FinalTime - app.FinalTime(1), "Format", "mm:ss.SSS"); %#ok
+            CounterVar = app.FinalCounter - app.Counter(1); %#ok
+            RawData = app.FinalData; %#ok
+            Accelerometer = app.FinalAccel; %#ok
+            Gyroscope = [app.FinalGyro app.FinalHighGyro]; %#ok
+            Magnetometer = app.FinalMag; %#ok
+            Velocity = app.Vel; %#ok
+            Orientation = app.Ornt'; %#ok
+            uisave({'TimeVar', 'CounterVar', 'RawData', 'Accelerometer', 'Gyroscope', 'Magnetometer', 'Velocity', 'Orientation'})
+        end
+
+        % Button pushed function: CloseButton
+        function CloseButtonPushed(app, event)
+            delete(app)
+        end
+    end
+
+    %% App GUI Functions
+    methods (Access = public)
+        function app = FilterAlgorithmApp()
+            % Button width and height
+            Width = 120;
+            Height = 40;
+            X = 20; % X-position (aligned to the left)
+            Spacing = 10; % Space between buttons
+            % createComponents(app);
+
+            app.UIFigure = uifigure('Name', 'Performance Tracking Hockey Puck App Results', ...
+                 'Position', [100, 100, 900, 700]);
+            app.SearchButton = uibutton(app.UIFigure, 'Text', 'Search', ...
+                 'Position', [X, 380, Width, Height], ...
+                 'ButtonPushedFcn', @(~,~) app.SearchButtonPushed());
+            app.ConnectButton = uibutton(app.UIFigure, 'Text', 'Connect', ...
+                 'Position', [X, 350 - (Height + Spacing), Width, Height], ...
+                 'ButtonPushedFcn', @(~,~) app.ConnectButtonPushed());
+            app.StartButton = uibutton(app.UIFigure, 'Text', 'Start', ...
+                 'Position', [X, 350 - 2*(Height + Spacing), Width, Height], ...
+                 'ButtonPushedFcn', @(~,~) app.StartButton());
+            app.StopButton = uibutton(app.UIFigure, 'Text', 'Stop', ...
+                 'Position', [X, 350 - 3*(Height + Spacing), Width, Height], ...
+                 'ButtonPushedFcn', @(~,~) app.getResults());
+            app.SaveButton = uibutton(app.UIFigure, 'Text', 'Save', ...
+                 'Position', [X, 350 - 4*(Height + Spacing), Width, Height], ...
+                 'ButtonPushedFcn', @(~,~) app.SaveButtonPushed());
+            app.Label = uilabel(app.UIFigure, 'Text', 'No file loaded', ...
+                 'Position', [20, 340, 200, 40]);
+            app.Axes = uiaxes(app.UIFigure, 'Position', [200, 50, 680, 600]);  % Larger graph area
+        end
+
+        % function createComponents(app)
+        %     % Create PuckSensorIMUSensorSystemUIFigure and hide until all components are created
+        %     app.UIFigure = uifigure('Visible', 'on');
+        %     app.UIFigure.Position = [100 100 640 480];
+        %     app.UIFigure.Name = 'Puck Sensor IMU Sensor System';
+        %     app.UIFigure.CloseRequestFcn = createCallbackFcn(app, @PuckSensorIMUSensorSystemUIFigureCloseRequest, true);
+        %     app.UIFigure.WindowState = 'maximized';
+        % 
+        %     % Create GridLayout
+        %     app.GridLayout = uigridlayout(app.UIFigure);
+        %     app.GridLayout.ColumnWidth = {'9x', '1x'};
+        %     app.GridLayout.RowHeight = {'1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x'};
+        % 
+        %     % Create ConnectButton
+        %     app.ConnectButton = uibutton(app.GridLayout, 'push');
+        %     app.ConnectButton.ButtonPushedFcn = createCallbackFcn(app, @ConnectButtonPushed, true);
+        %     app.ConnectButton.FontSize = 14;
+        %     app.ConnectButton.Enable = 'off';
+        %     app.ConnectButton.Layout.Row = 4;
+        %     app.ConnectButton.Layout.Column = 2;
+        %     app.ConnectButton.Text = 'Connect';
+        % 
+        %     % Create DisconnectButton
+        %     app.DisconnectButton = uibutton(app.GridLayout, 'push');
+        %     app.DisconnectButton.ButtonPushedFcn = createCallbackFcn(app, @DisconnectButtonPushed, true);
+        %     app.DisconnectButton.FontSize = 14;
+        %     app.DisconnectButton.Enable = 'off';
+        %     app.DisconnectButton.Layout.Row = 5;
+        %     app.DisconnectButton.Layout.Column = 2;
+        %     app.DisconnectButton.Text = 'Disconnect';
+        % 
+        %     % Create StartButton
+        %     app.StartButton = uibutton(app.GridLayout, 'push');
+        %     app.StartButton.ButtonPushedFcn = createCallbackFcn(app, @StartButtonPushed, true);
+        %     app.StartButton.FontSize = 14;
+        %     app.StartButton.Enable = 'off';
+        %     app.StartButton.Layout.Row = 11;
+        %     app.StartButton.Layout.Column = 2;
+        %     app.StartButton.Text = 'Start';
+        % 
+        %     % Create StopButton
+        %     app.StopButton = uibutton(app.GridLayout, 'push');
+        %     app.StopButton.ButtonPushedFcn = createCallbackFcn(app, @getResults, true);
+        %     app.StopButton.FontSize = 14;
+        %     app.StopButton.Enable = 'off';
+        %     app.StopButton.Layout.Row = 12;
+        %     app.StopButton.Layout.Column = 2;
+        %     app.StopButton.Text = 'Finish';
+        % end
     end
 end
